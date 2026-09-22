0f0c906d51bf6f81f79c2ab408ff9f9a3555c60c
Fi <fomakotik78@gmail.com>
2026-09-22 17:34:33 +0300

    Я обязательно выживу

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -40,7 +40,7 @@ kotlin {
         }
 
         commonMain.dependencies {
-            implementation(libs.ktor.client.core)
+            api(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
             implementation(libs.kotlinx.serialization.json)
